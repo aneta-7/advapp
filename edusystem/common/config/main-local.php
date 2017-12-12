e20d98c9768e1c927dc910ae156a074118c0a9d1
Michal <gomoslaw@gmail.com>
2017-12-12 12:16:47 +0100

    initial application skeleton

---
+++ edusystem/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=edusystem',
+            'username' => 'michal',
+            'password' => 'zabski',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
---
+++ edusystem/frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'A02Ms9pro_thmgIkIzTKKLjJ2kOND9oO',
+        ],
+    ],
+];
